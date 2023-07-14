f248a04a20e868816c53bbf2980a0be1bd408a81
avasa86 <14539@coderacademy.edu.au>
2023-07-14 21:28:00 +1000

    Wrote update function for student successfullly

--- src/database.py
+++ src/database.py
@@ -45,7 +45,7 @@ def insert_subject(subject_name):
 
     try:
         # Insert the contact into the table
-        c.execute("INSERT INTO subject (subject_name) VALUES (?)",(subject_name))
+        c.execute("INSERT INTO subject (subject_name) VALUES (?)",(subject_name,))
 
         # Commit the changes
         conn.commit()
@@ -79,8 +79,27 @@ def insert_student(first_name,last_name,email,age,phone_number,parents_phone_num
         conn.close()
 
 
-def update_student(first_name,last_name,email,age,phone_number,parents_phone_number,mark,subject_id):
-    pass
+def update_student(first_name,last_name,email,age,phone_number,parents_phone_number,mark,subject_id,ids):
+    # Connect to the SQLite database
+    conn = sqlite3.connect('studentdatabase.db')
+    c = conn.cursor()
+
+    try:
+        # Update the student
+        c.execute("UPDATE student SET first_name = ?,last_name = ?,email = ?,age = ?,phone_number = ?,parents_phone_number = ?,mark = ?,subject_id = ? WHERE id = ?", (first_name,last_name,email,age,phone_number,parents_phone_number,mark,subject_id,ids))
+
+        # Commit the changes
+        conn.commit()
+        print("Student updated successfully!")
+
+    except sqlite3.Error as e:
+        print("Error updating student details:", e)
+
+    finally:
+        # Close the connection
+        conn.close()
+
+
 
 
 def update_subject(subject_name,subject_id):
@@ -104,13 +123,55 @@ def update_subject(subject_name,subject_id):
         conn.close()
 
 
-def delete_student():
-    pass
+def delete_student(record_id):
+    # Connect to the SQLite database
+    conn = sqlite3.connect('studentdatabase.db')
+    c = conn.cursor()
+
+    try:
+        # Delete the record from the table
+        c.execute("DELETE FROM student WHERE id = ?", (record_id))
+
+        # Commit the changes
+        conn.commit()
+        print("Record deleted successfully!")
+
+    except sqlite3.Error as e:
+        print("Error deleting record:", e)
+
+    finally:
+        # Close the connection
+        conn.close()    
 
 def delete_subject():
-    pass
+    # Connect to the SQLite database
+    conn = sqlite3.connect('studentdatabase.db')
+    c = conn.cursor()
+
+    try:
+        # Delete the student from the table
+        c.execute("DELETE FROM subject WHERE subject_name = ?", (subject_name,))
+
+        # Commit the changes
+        conn.commit()
+        print("Record deleted successfully!")
+
+    except sqlite3.Error as e:
+        print("Error deleting record:", e)
+
+    finally:
+        # Close the connection
+        conn.close()    
+  
+
 create_table()
 insert_subject('Maths')
 insert_subject('Physics')
-insert_student("Avas","Ali","aa@gmail.com",29,"0444444444","0455555555",98,1)
+insert_subject('Chemistry')
+insert_subject('Economics')
+insert_student("Avas","Ali","aa@gmail.com",29,"0444444444","0455551255",98,1)
+insert_student("Aban","Bad","ab@gmail.com",30,"04445986","0455555235",95,2)
+insert_student("Cat","Dinoson","ac@gmail.com",35,"0444442344","0445555555",93,3)
+insert_student("Aprer","Aci","ad@gmail.com",36,"0445644444","0458955555",91,4)
+update_student("Aprer","Aci","ad@gmail.com",36,"0445644444","0458955555",69,1,4)
 
