e0c5c870618363e6c4e13861640c8b08bca18d94
avasa86 <14539@coderacademy.edu.au>
2023-07-14 19:30:19 +1000

    Wrote class in wrong file. Placed in the correct file database.py

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Student Management System
-The following application is a Student Management System written in python that solves some of the key problems we face when managing student data.
+The following application is a Student Management System written in python that solves some of the key problems we face when managing student data.We will be using a user-friendly CLI that will deal with such systems.
 - **Link To Repository:** https://github.com/avasa86/AvasMohammadAli_T1A3
 - **Coding style guide:** PEP8 style coding
+
--- src/studentcli.py
+++ src/studentcli.py
@@ -1,32 +1,22 @@
 import typer
+from rich.console import Console
+from rich.table import Table
+from model import Todo
+from database import get_all_todos, delete_todo, insert_todo, complete_todo, update_todo
+
+console = Console()
 
 app = typer.Typer()
 
 
 @app.command(short_help='adds an item')
-def add(first_name: str, last_name: str, email: str,phno:str):
-    typer.echo(f"Adding details: {first_name}, {last_name}, {email},{phno}")
-
-
-@app.command()
-def delete(position: int):
-    typer.echo(f"deleting {position}")
-
+def add(task: str, category: str):
+    pass
 
 @app.command()
+def delete(position: int): ##removes an item
+    pass
+
+@app.command() ##updates an item
 def update(position: int, task: str = None, category: str = None):
-    typer.echo(f"updating {position}")
-
-
-@app.command()
-def complete(position: int):
-    typer.echo(f"complete {position}")
-
-
-@app.command()
-def show():
-    typer.echo(f"Todos")
-
-
-if __name__ == "__main__":
-    app()
+    pass
